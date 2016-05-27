4a5b0673da44aac164ddacf61c87027cbbcef0a5
Luke Deen Taylor <ldt@deentaylor.com>
2016-05-26 22:49:33 -0400

    Update __init__.py

--- ui2/__init__.py
+++ ui2/__init__.py
@@ -1,1 +1,9 @@
+""" A module that builds on Pythonista's `ui` module """
+
+# Load subpackages
 import shapes
+import ui_io
+
+# Dump ui into namespace so that `import ui2` can be used interchangably with
+# `import ui`
+from ui import *
