966e186540badc3dd449be2054935c0de25e0b95
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-06-14 15:25:01 -0400

    Add method for copying paths

--- ui2/path_helpers.py
+++ ui2/path_helpers.py
@@ -7,3 +7,15 @@ def get_path_image(path):
     with ui.ImageContext(bounds.max_x, bounds.max_y) as ctx:
         path.fill()
         return ctx.get_image()
+
+
+def copy_path(path):
+    """ Make a copy of a ui.Path and return it. Preserves all data. """
+    new = ui.Path()
+    new.append_path(path)
+    # Copy over the attributes
+    new.line_cap_style = path.line_cap_style
+    new.line_join_style = path.line_join_style
+    new.line_width = path.line_width
+
+    return new
