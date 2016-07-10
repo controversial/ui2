8ae2b4230ddb3b0a3b9b1843cece8c4e703543cc
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-10 16:20:02 -0400

    Add ChainedAnimation class

--- ui2/animate.py
+++ ui2/animate.py
@@ -30,6 +30,32 @@ class Animation(object):
         )
 
 
+class ChainedAnimationComponent(Animation):
+    def __init__(self, animation, next_animation):
+        self.animation_obj = animation
+        self.next_animation = next_animation
+
+        self.duration = self.animation_obj.duration
+        self.delay = self.animation_obj.delay
+        self.animation = self.animation_obj.animation
+
+    def completion(self, success):
+        self.animation_obj.completion(success)
+        if self.next_animation is not None:
+            self.next_animation.play()
+
+
+class ChainedAnimation(object):
+    def __init__(self, *animations):
+        self.anims = [ChainedAnimationComponent(
+                      a,
+                      animations[i + 1] if i < len(animations) - 1 else None
+                      ) for i, a in enumerate(animations)]
+
+    def play(self):
+        self.anims[0].play()
+
+
 def animate(animation, duration=0.25, delay=0.0, completion=None):
     """A drop-in replacement for ui.animate"""
     Animation(animation, duration, delay, completion).play()
