540809e064290c38b55f54a457d3e864a1f64b97
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-10 21:38:23 -0400

    Support the built-in easings on animate

--- ui2/animate.py
+++ ui2/animate.py
@@ -1,15 +1,25 @@
+import warnings
+
 from objc_util import *
 
 
-# Basic animation class
+# Constants representing easings. See http://apple.co/29FOF5i
+
+ANIMATE_EASE_IN = 1 << 16
+ANIMATE_EASE_OUT = 2 << 16
+ANIMATE_EASE_IN_OUT = 0 << 16
+ANIMATE_EASE_NONE = ANIMATE_LINEAR = 3 << 16
+
 
 class Animation(object):
     """Represents an animation to one or more properties of an object."""
-    def __init__(self, animation, duration=0.25, delay=0.0, completion=None):
+    def __init__(self, animation, duration=0.25, delay=0.0, completion=None,
+                 easing=ANIMATE_EASE_IN_OUT):
         self.animation = animation
         self.duration = duration
         self.delay = delay
         self.completion = completion
+        self.easing = easing
 
     def play(self):
         """Perform the animation."""
@@ -23,19 +33,24 @@ class Animation(object):
             oncomplete = None
 
         UIView.animateWithDuration_delay_options_animations_completion_(
-            self.duration, self.delay, 0, ObjCBlock(self.animation), oncomplete
+            self.duration,
+            self.delay,
+            self.easing,
+            ObjCBlock(self.animation),
+            oncomplete
         )
 
 
 class ChainedAnimationComponent(Animation):
-    """A single step in a chain of animations."""
-    def __init__(self, animation, next_animation):
-        self.animation_obj = animation
+    def __init__(self, animation_obj, next_animation):
+        """A single step in a chain of animations."""
+        self.animation_obj = animation_obj
         self.next_animation = next_animation
 
         self.duration = self.animation_obj.duration
         self.delay = self.animation_obj.delay
         self.animation = self.animation_obj.animation
+        self.easing = self.animation_obj.easing
 
     def completion(self, success):
         self.animation_obj.completion(success)
@@ -57,5 +72,8 @@ class ChainedAnimation(object):
 
 
 def animate(animation, *args, **kwargs):
-    """A drop-in replacement for ui.animate."""
+    """A drop-in replacement for ui.animate.
+
+    This adds support for different easings.
+    """
     Animation(animation, *args, **kwargs).play()
